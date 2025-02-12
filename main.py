5f0fadcfd7cc4059ef325f93bbacf65dbc6a6f7a
halasjakub <83594926+halasjakub@users.noreply.github.com>
2025-02-12 22:54:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -72,13 +72,9 @@ menu_bar.add_command(label="Explore", command=get_stock_data)
 window.grid_columnconfigure(0, weight=1, minsize=100)
 window.grid_columnconfigure(1, weight=0, minsize=100)
 
-# Title label
-label_title = tk.Label(window, text="Stock Price", font=("Arial", 50), anchor="w")
-label_title.grid(row=0, column=0, padx=10, pady=0, sticky="w")
-
 # Time label (current time)
 label_time = tk.Label(window, font=("Arial", 14), anchor="w")
-label_time.grid(row=1, column=0, padx=10, pady=0, sticky="w")
+label_time.grid(row=0, column=0, padx=10, pady=0, sticky="w")
 
 # Information label about the stock data source
 label_info = tk.Label(
@@ -94,15 +90,15 @@ label_info = tk.Label(
     justify="left",
     wraplength=350,
 )
-label_info.grid(row=2, column=0, padx=10, pady=(5, 10), sticky="w")
+label_info.grid(row=1, column=0, padx=10, pady=(5, 10), sticky="w")
 
 # Company code input label
 label_company_code = tk.Label(window, text="Enter stock company code:", font=("Arial", 10))
-label_company_code.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+label_company_code.grid(row=2, column=0, padx=10, pady=5, sticky="w")
 
 # Company code input field
 entry_company_code = tk.Entry(window, font=("Arial", 10))
-entry_company_code.grid(row=4, column=0, padx=10, pady=5, sticky="w")
+entry_company_code.grid(row=3, column=0, padx=10, pady=5, sticky="w")
 
 # Label to indicate pricing delay
 label_delayed = tk.Label(window, text="Pricing delayed", font=("Arial", 14), anchor="e")
@@ -114,12 +110,12 @@ label_stock_current_price.grid(row=1, column=1, padx=10, pady=0, sticky="ne")
 
 # Frame for displaying the chart
 frame_chart = tk.Frame(window)
-frame_chart.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
+frame_chart.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
 
 # Slider for selecting the time period
 period_slider = tk.Scale(window, from_=1, to=365, orient="horizontal", label="Select Period:", font=("Arial", 10))
 period_slider.set(30)
-period_slider.grid(row=5, column=0, padx=10, pady=5, sticky="w")
+period_slider.grid(row=4, column=0, padx=10, pady=5, sticky="w")
 
 # Update the current time every second
 update_time()
